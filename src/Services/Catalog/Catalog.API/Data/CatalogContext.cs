591e8c87c9edc9a91959b7c397f629f19daed2f4
AllwinFranklin <allwinanto360@gmail.com>
2023-11-18 21:25:45 +0530

    Discount API added

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -5,6 +5,8 @@ namespace Catalog.API.Data
 {
     public class CatalogContext : ICatalogContext
     {
+        public IMongoCollection<Product> Products {  get; }
+
         public CatalogContext(IConfiguration configuration)
         {
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
@@ -13,7 +15,5 @@ namespace Catalog.API.Data
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
             CatalogContextSeed.SeedData(Products);
         }
-
-        public IMongoCollection<Product> Products {  get; }
     }
 }

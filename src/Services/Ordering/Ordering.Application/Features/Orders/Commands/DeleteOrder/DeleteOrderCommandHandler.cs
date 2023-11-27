217e7f44491437a6d94d0a778972a5338c5fcd28
AllwinFranklin <allwinanto360@gmail.com>
2023-11-27 19:03:42 +0530

    Ordering service added and in progress

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    internal class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+            await _orderRepository.DeleteAsync(orderToDelete);
+            if (orderToDelete == null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+            _mapper.Map(request, orderToDelete, typeof(UpdateOrderCommand), typeof(Order));
+
+            await _orderRepository.DeleteAsync(orderToDelete);
+
+            _logger.LogInformation($"Order {orderToDelete.Id} is successfully deleted.");
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrersListQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrderByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}

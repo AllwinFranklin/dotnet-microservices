c8afe37e11a9a2fb743205b45481abc273caec52
AllwinFranklin <allwinanto360@gmail.com>
2024-01-04 17:58:34 +0530

    Added entity framework and now books are now queriable

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,15 +1,18 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
 
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json");
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
 
 await app.UseOcelot();
 
